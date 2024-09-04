69ab631c61cfa9dce0f0386fda7f0deb19189034
being-aryan <134589308+being-aryan@users.noreply.github.com>
2024-09-04 23:12:40 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -5,8 +5,8 @@ contract ERC20 {
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint)) public allowance;
-    string public name = "Tekken";
-    string public symbol = "TKN";
+    string public name = "beings";
+    string public symbol = "DOPA";
     uint8 public decimals = 18;
 
     event Transfer(address indexed from, address indexed to, uint value);
